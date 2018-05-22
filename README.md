c41e33401a5c0312eef33dd0b3b7b2d99fe97c81
Ewan Bauld <ewanbauld@Ewans-iMac-2.local>
2018-05-22 23:03:46 +0100

    Init Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Boilerplate Application
+
+Boilerplate setup using React JS
+
+This is project has been written in React JS, using Webpack and Babel. 
+
+This project includes React-Redux, React-Router and has been tested using Enzyme and Jest
+
+It uses a Firebase web based database.
+
+The styling has been carried out using Sass.
+
+
---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter, { history } from './routers/AppRouter';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+import LoadingPage from './components/LoadingPage';
+
+
+// const jsx = (
+//     <AppRouter />
+// );
+// let hasRendered = false;
+// const renderApp = ()=> {
+//   // if (!hasRendered) {
+//       ReactDOM.render(jsx, document.getElementById('app'));
+//       // hasRendered = true;
+//   // }
+// };
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
