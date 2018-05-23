4f268c1336100f2a689af7c2f41d89838efc6e1d
Ewan Bauld <ewan.bauld@gmail.com>
2018-05-23 17:22:53 +0100

    Add Mocha/Chai/SuperTest for API

--- test/test-profile.js
+++ test/test-profile.js
@@ -1,20 +1,42 @@
 const should = require('chai').should(),
     expect = require('chai').expect,
     supertest = require('supertest'),
-    api = supertest('http://localhost:5000/api/profiles');
+    api = supertest('http://localhost:5000/api/profile');
 
 
 describe('Profile function', () => {
 
-    it('should get a test profile', (done) => {
-        api.get('/profile')
+    it('should GET profile by the handle', (done) => {
+        api.get('/handle/ewanbauld')
             .set('Accept', 'application/json')
             .expect(200, done);
     });
 
     it('should return a 404 when the route does not exist', (done) => {
-        api.get('/notfound')
+        api.get('/handle/unknown')
             .set('Accept', 'application/json')
             .expect(404, done);
-    })
+    });
+
+    it('should make a valid POST request', () => {
+        let handle1;
+        api.post('/')
+            .set('Accept', 'application/x-www-form-urlencoded')
+            .send({
+                handle: 'ewanbauld',
+                company: "DHL",
+                website: "www.thought.com",
+                location: "Manchester",
+                bio: "About me of course",
+                status: "Web Dev",
+                githubusername: "Testy",
+                skills: "HTML,CSS"
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                handle1 = res.body
+            })
+
+    });
 });
