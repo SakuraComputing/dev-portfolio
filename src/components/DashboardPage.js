bd34fad4efd7a282c6b841f4a74ca1c6aafc75c3
Ewan Bauld <ewanbauld@Ewans-iMac-2.local>
2018-05-23 01:02:12 +0100

    Further styling - template in place

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -17,23 +17,29 @@ import CreditsAside from '../components/CreditsAside';
 import Footer from '../components/Footer';
 
 const DashboardPage = () =>(
-    <div>
+    <body>
         <Header />
-        <About/>
-        <LatestProject/>
-        <OtherProjects/>
-        <Experience/>
-        <Github/>
-        <InfoAside/>
-        <SkillsAside/>
-        <TestimonialsAside/>
-        <EducationAside/>
-        <LanguagesAside/>
-        <BlogAside/>
-        <MusicAside/>
-        <ConferenceAside/>
-        <CreditsAside/>
-        <Footer/>
-    </div>
+        <div className="container sections-wrapper">
+            <div className="row">
+                <div className="primary col-md-8 col-sm-12 col-xs-12">
+                    <About/>
+                    <LatestProject/>
+                    <OtherProjects/>
+                    <Experience/>
+                    <Github/>
+                    <InfoAside/>
+                    <SkillsAside/>
+                    <TestimonialsAside/>
+                    <EducationAside/>
+                    <LanguagesAside/>
+                    <BlogAside/>
+                    <MusicAside/>
+                    <ConferenceAside/>
+                    <CreditsAside/>
+                    <Footer/>
+                </div>
+            </div>
+        </div>
+    </body>
 );
 export default DashboardPage;
