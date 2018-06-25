0f63c62b74b6a47b0d37f3752f54cb3d724169c5
Ewan Bauld <ewan.bauld@gmail.com>
2018-06-25 20:36:39 +0900

    Clean up app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter, { history } from './routers/AppRouter';
+import DashBoardPage from './components/DashboardPage';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(<DashBoardPage />, document.getElementById('app'));
