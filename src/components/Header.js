486a082d88263ea413b2d4fe6562ddba3292b968
Ewan Bauld <ewan.bauld@gmail.com>
2018-06-26 22:59:58 +0900

    Refactor Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,16 @@ export const Header = (props) => (
 
     <header className="header">
         <div className="container">
-            <img className="profile-image img-responsive pull-left" src="/public/images/loader.gif" alt="Ewan Bauld"/>
+            <img className="profile-image img-responsive pull-left" src="/vpublic/images/bogu.jpg" alt="Ewan Bauld"/>
             <div className="profile-content pull-left">
                 <h1 className="name">{props.header.name}</h1>
                 <h2 className="desc">{props.header.title}</h2>
                 <ul className="social list-inline">
-                    {props.header.social.twitter.url ? <li><a href="#"><i className="fa fa-twitter"></i></a></li> : null}
-                    {props.header.social.googleplus.url ?<li><a href="#"><i className="fa fa-google-plus"></i></a></li> : null}
-                    {props.header.social.linkedin.url ? <li><a href="#"><i className="fa fa-linkedin"></i></a></li> : null}
-                    {props.header.social.github.url ? <li><a href="#"><i className="fa fa-github-alt"></i></a></li> : null}
-                    {props.header.social.github.url ? <li className="last-item"><a href="#"><i className="fa fa-hacker-news"></i></a></li> : null}
+                    {props.header.social.twitter.url ? <li><a href={props.header.social.twitter.url}><i className="fa fa-twitter"></i></a></li> : null}
+                    {props.header.social.googleplus.url ?<li><a href={props.header.social.googleplus.url}><i className="fa fa-google-plus"></i></a></li> : null}
+                    {props.header.social.linkedin.url ? <li><a href={props.header.social.linkedin.url}><i className="fa fa-linkedin"></i></a></li> : null}
+                    {props.header.social.github.url ? <li><a href={props.header.social.github.url}><i className="fa fa-github-alt"></i></a></li> : null}
+                    {props.header.social.hackernews.url ? <li className="last-item"><a href={props.header.social.hackernews.url}><i className="fa fa-hacker-news"></i></a></li> : null}
                 </ul>
             </div>
             <a className="btn btn-cta-primary pull-right" href="http://themes.3rdwavemedia.com/" target="_blank"><i
