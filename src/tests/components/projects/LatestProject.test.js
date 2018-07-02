ad06ed63cb187afd8a1a491196f150c20473c648
Ewan Bauld <ewan.bauld@gmail.com>
2018-07-02 18:40:09 +0900

    Data model for projects

--- src/tests/components/projects/LatestProject.test.js
+++ src/tests/components/projects/LatestProject.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import LatestProject from '../../components/LatestProject';
+import LatestProject from '../../../components/projects/LatestProject';
 
 describe('Latest Project Section', () => {
     it('should render correctly', () => {
