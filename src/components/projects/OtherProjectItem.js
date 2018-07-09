94820f2d1e131982d3f993cdcea248ede3dd5310
Ewan Bauld <ewan.bauld@gmail.com>
2018-07-09 17:55:10 +0100

    Fix Other project item re-direction to github

--- src/components/projects/OtherProjectItem.js
+++ src/components/projects/OtherProjectItem.js
@@ -8,7 +8,7 @@ const OtherProjectItem = (props) => {
                     className="label label-theme">Open Source</span></h3>
                 <p className="summary">{props.otherproject.description}</p>
                 <p><a className="more-link"
-                      href="https://github.com/SakuraComputing/embDocs"
+                      href={props.otherproject.github}
                       target="_blank"><i className="fa fa-external-link"></i> View on GitHub</a></p>
             </div>
         </div>
