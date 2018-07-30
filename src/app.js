6dfd50f4f107df611bd0f387ca8b9136c482a1a4
Ewan Bauld <ewan.bauld@gmail.com>
2018-07-31 00:34:30 +0100

    Clean up project

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LandingPage from './components/LandingPage';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 
 ReactDOM.render(<LandingPage />, document.getElementById('app'));
